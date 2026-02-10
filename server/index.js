e1c49bc89390abc3303308aef2d0a3639b1003cc
Yamuna-mummala <yamunamummala@gmail.com>
2026-02-10 15:19:40 +0530

    Add User model schema for authentication

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
 const app = express();
 
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
